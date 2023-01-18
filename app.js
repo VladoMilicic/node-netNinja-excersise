e92d59a427203e9f1b7b9869c69e48c9eb49776e
VladoMilicic <vlado_milicic@hotmail.com>
2023-01-18 18:13:36 +0100

    added staticfiles routing and now to use next() in middleware

--- app.js
+++ app.js
@@ -11,11 +11,28 @@ app.set('view engine', 'ejs') //configure app setings this time ejs view engine 
 //listen for requests
 app.listen(3008)
 
+//static files
+app.use(express.static('public'))
+
+//custom middleware
+app.use((req, res, next) => {
+    console.log("new request made: ")
+    console.log("host: ", req.hostname)
+    console.log("path: ", req.path)
+    console.log("method: ", req.method)
+    next();//next funkcija govori da se ne zaustavi kad izvrsi ovaj mddleware nego da ide dalje krozkod
+});
+
 //listening get requests
 app.get('/', (req, res) => {
     // res.send('<p>Home page</p>')
     //res.sendFile('./views/index.html', { root: __dirname })
-    res.render('index', { title: "Home" }); //views rendering sa ejs umijesto starog nacina sa sendFile
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
+    res.render('index', { title: "Home", blogs }); //views rendering sa ejs umijesto starog nacina sa sendFile
 })
 app.get('/about', (req, res) => {
     // res.send('<p>Home page</p>')
